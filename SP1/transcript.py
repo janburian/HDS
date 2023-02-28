b31a758d5062088f9bb3f4e2b83fe9e23879a8d7
janburian <jburian@students.zcu.cz>
2023-02-28 17:13:34 +0100

    Added declaration of method

--- SP1/transcript.py
+++ SP1/transcript.py
@@ -1,7 +1,7 @@
 from pathlib import Path
 import os
 
-def load_input_txt_file(filename: str):
+def read_input_file(filename: str):
     sentences_list = []
     with open(filename, 'r', encoding='utf-8') as file:
         for line in file:
@@ -11,10 +11,12 @@ def load_input_txt_file(filename: str):
 
     return sentences_list
 
+def save_output_file(filename: str):
+    pass
 
 if __name__ == "__main__":
     filename = "vety_HDS.ortho.txt"
-    sentences_list = load_input_txt_file(filename)
+    sentences_list = read_input_file(filename)
     print()
 
 
