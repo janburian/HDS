69d44f4b70ca6591c89f294b53cd94d5539b5522
janburian <jburian@students.zcu.cz>
2023-04-06 15:22:05 +0200

    Preparing data for second term paper

---
+++ SP2/phonetic_transcription.py
@@ -0,0 +1,234 @@
+import rules
+from pathlib import Path
+
+
+def read_input_file(filename: Path):
+    sentences_dict = {}
+    with open(filename, 'r', encoding='utf-8-sig') as file:
+        for line in file:
+            element = line.split(';')
+            key = element[0].strip()
+            sentence = element[1].strip()
+
+            sentences_dict[key] = sentence
+    file.close()
+
+    return sentences_dict
+
+
+def save_output_file(sentences_dict: dict, filename: str):
+    f = open(filename, 'w', encoding='utf-8')
+
+    for key in sentences_dict:
+        sentence = sentences_dict[key] + '\n'
+        f.write(key + '|' + sentence)
+
+    f.close()
+
+
+def apply_basic_rules(sentences_dict: dict, basic_rules: dict):
+    res = {}
+    for key in sentences_dict:
+        sentence = sentences_dict[key]
+        pause = '$ '
+        sentence = pause + sentence.lower()
+        for char in basic_rules:
+            char_transcript = basic_rules[char]
+            sentence = sentence.replace(char, char_transcript)
+
+        res[key] = sentence
+
+    return res
+
+
+def apply_alophones(sentences_dict: dict):
+    # alophones = ['n', 'm', 'R', 'r', 'l']
+    # consonants_all = rules.CONSONANTS_ALL
+    consonants_voiceless = rules.VOICELESS_CONSONANTS_PAIR
+    res = {}
+
+    for key in sentences_dict:
+        sentence = sentences_dict[key]
+        words = sentence.split(' ')
+        words_temp = []
+        for word in words:
+            if 'n' in word:
+                word = change_char(word, 'n', next_chars=['k', 'g'], replacement='N')
+            if 'm' in word:
+                word = change_char(word, 'm', next_chars=['v', 'f'], replacement='M')
+            # if 'm' in word:
+            #     word = change_char_between_consonants(word, 'm', consonants_all, replacement='H')
+            # if 'r' in word:
+            #     word = change_char_between_consonants(word, 'r', consonants_all, replacement='P')
+            # if 'l' in word:
+            #     word = change_char_between_consonants(word, 'l', consonants_all, replacement='L')
+            if 'R' in word:
+                word = change_char_between_consonants_2(word, 'R', consonants_voiceless, replacement='Q')
+
+            words_temp.append(word)
+
+        sentence = ' '.join(words_temp)
+        res[key] = sentence
+
+    return res
+
+
+def change_char_between_consonants_2(word: str, char: str, consonants: list, replacement: str):
+    char_idx = word.index(char)
+    if char_idx == len(word) - 1:
+        word_list = list(word)
+        word_list[char_idx] = replacement
+        word = ''.join(word_list)
+
+        return word
+
+    if char_idx > 0 and word[char_idx - 1] in consonants:
+        word_list = list(word)
+        word_list[char_idx] = replacement
+        word = ''.join(word_list)
+
+        return word
+
+    return word
+
+
+def change_char_between_consonants(word: str, char: str, consonants: list, replacement: str):
+    char_idx = word.index(char)
+    if char_idx == len(word) - 1 and word[char_idx - 1] in consonants:
+        word_list = list(word)
+        word_list[char_idx] = replacement
+        word = ''.join(word_list)
+
+        return word
+
+    if char_idx > 0 and word[char_idx - 1] in consonants and word[char_idx + 1] in consonants:
+        word_list = list(word)
+        word_list[char_idx] = replacement
+        word = ''.join(word_list)
+
+        return word
+
+    return word
+
+
+def change_char(word: str, char: str, next_chars: list, replacement: str):
+    char_idx = word.index(char)
+    if char_idx < len(word) - 2:
+        next_phoneme = word[char_idx + 1]
+        if next_phoneme in next_chars:
+            word_list = list(word)
+            word_list[char_idx] = replacement
+            word = "".join(word_list)
+
+    return word
+
+
+def apply_assimilation(sentences_dict: dict):
+    res = {}
+    special_chars = ['#', '!', '$']
+    for key in sentences_dict:
+        sentence = sentences_dict[key]
+
+        for i in range(len(sentence)-2, 1, -1):  # reversed for cycle (from the end to the beginning of the sentence)
+            current_char = sentence[i]
+            prev_char = sentence[i + 1]
+
+            sentence = assimilation_voiced_consonants(current_char, i, prev_char, sentence, special_chars)
+            sentence = assimilation_voiceless_consonants(current_char, i, prev_char, sentence)
+
+        res[key] = sentence
+
+    return res
+
+
+def assimilation_voiceless_consonants(current_char: str, i: int, prev_char: str, sentence: str):  # assimilation - voiceless consonants
+    if current_char in rules.VOICELESS_CONSONANTS_PAIR and (
+            prev_char in rules.VOICED_CONSONANTS_PAIR or prev_char == ' '):
+        if prev_char == ' ' and i > 0:
+            char_after_vertical_bar = sentence[i + 2]
+            if (char_after_vertical_bar in rules.VOICED_CONSONANTS_PAIR) and char_after_vertical_bar != 'v':
+                current_char_idx = i
+                new_char = rules.VOICELESS_CONSONANTS_PAIR_to_VOICED_CONSONANTS_PAIR[current_char]
+                sentence = sentence[:current_char_idx] + new_char + sentence[current_char_idx + 1:]
+        elif prev_char != 'v':  # special case
+            current_char_idx = i
+            new_char = rules.VOICELESS_CONSONANTS_PAIR_to_VOICED_CONSONANTS_PAIR[current_char]
+            sentence = sentence[:current_char_idx] + new_char + sentence[current_char_idx + 1:]
+
+    return sentence
+
+
+def assimilation_voiced_consonants(current_char: str, i: int, prev_char: str, sentence: str, special_chars: list):  # assimilation - voiced consonants
+    char_after_vertical_bar = ''
+    if current_char in rules.VOICED_CONSONANTS_PAIR and (
+            prev_char in rules.VOICELESS_CONSONANTS_PAIR or prev_char == ' '):
+        if prev_char == ' ' and i > 0:
+            char_after_vertical_bar = sentence[i + 2]
+            if (char_after_vertical_bar in rules.VOWELS or char_after_vertical_bar in rules.VOICELESS_CONSONANTS_PAIR or
+                    char_after_vertical_bar in special_chars or char_after_vertical_bar in rules.UNIQUE_CONSONANTS or char_after_vertical_bar == 'v'):
+                current_char_idx = i
+                new_char = rules.VOICED_CONSONANTS_PAIR_to_VOICELESS_CONSONANTS_PAIR[current_char]
+                sentence = sentence[:current_char_idx] + new_char + sentence[current_char_idx + 1:]
+        elif char_after_vertical_bar != 'v':
+            current_char_idx = i
+            new_char = rules.VOICED_CONSONANTS_PAIR_to_VOICELESS_CONSONANTS_PAIR[current_char]
+            sentence = sentence[:current_char_idx] + new_char + sentence[current_char_idx + 1:]
+
+    return sentence
+
+
+def check_prepositions(sentences_dict: dict):
+    res = {}
+    # 'bez', 'nad', 'ob', 'od', 'pod', 'pQed' monosyllabic prepositions (end with voiced pair consonants)
+    # 'k', 's', 'v', 'z' disyllabic prepositions
+    prepositions_bad = ['bes', 'nat', '!op', '!ot', 'pot', 'pQet', 'g', 'f', 's']
+    dict_prepositions = {
+        'bes': 'bez',
+        'nat': 'nad',
+        '!op': '!ob',
+        '!ot': '!od',
+        'pot': 'pod',
+        'pQet': 'pQed',
+        'g': 'k',
+        'f': 'v',
+        's': 'z',
+    }
+
+    for key in sentences_dict:
+        sentence = sentences_dict[key]
+        words = sentence.split(' ')
+        for preposition_bad in prepositions_bad:
+            if preposition_bad in words:
+                preposition_idx = words.index(preposition_bad)
+                if preposition_idx < len(words)-1:
+                    word_after_preposition = words[preposition_idx + 1]
+                    first_char = word_after_preposition[0]
+                    if first_char in rules.UNIQUE_CONSONANTS or first_char == 'v':
+                        words[preposition_idx] = dict_prepositions[preposition_bad]
+
+        sentence = ' '.join(words)
+        res[key] = sentence
+
+    res = check_s_preposition_personal_pronouns(res)
+
+    return res
+
+def check_s_preposition_personal_pronouns(sentences_dict: dict):
+    res = {}
+    personal_pronouns = rules.PERSONAL_PRONOUNS
+    bad_preposition = 'z'
+
+    for key in sentences_dict:
+        sentence = sentences_dict[key]
+        words = sentence.split(' ')
+        if bad_preposition in words:
+            preposition_idx = words.index(bad_preposition)
+            if preposition_idx < len(words) - 1:
+                word_after_preposition = words[preposition_idx + 1]
+                if word_after_preposition in personal_pronouns:
+                    words[preposition_idx] = 's'
+
+        sentence = ' '.join(words)
+        res[key] = sentence
+
+    return res
---
+++ SP2/rules.py
@@ -0,0 +1,159 @@
+# EPA alphabet,...
+BASIC_RULES = {
+    'di': 'Di',
+    'dí': 'DI',
+    'ti': 'Ti',
+    'tí': 'TI',
+    'ni': 'Ji',
+    'ní': 'JI',
+
+    'dě': 'De',
+    'tě': 'Te',
+    'ně': 'Je',
+    'mě': 'mJe',
+    'bě': 'bje',
+    'pě': 'pje',
+    'vě': 'vje',
+
+    'ě': 'je',
+
+    'js': 's',
+
+    # vocals
+    'a': 'a',
+    'e': 'e',
+    'i': 'i',
+    'o': 'o',
+    'u': 'u',
+
+    'á': 'A',
+    'é': 'E',
+    'í': 'I',
+    'ó': 'O',
+    'ú': 'U',
+    'ů': 'U',
+
+    'y': 'i',
+    'ý': 'I',
+
+    # diphtongs
+    'ou': 'y',
+    'au': 'Y',
+    'eu': 'F',
+
+    'x': 'ks',
+
+    # fricatives
+    'f': 'f',
+    'v': 'v',
+    's': 's',
+    'z': 'z',
+    'š': 'S',
+    'ž': 'Z',
+    'ch': 'x',
+    'h': 'h',
+    'l': 'l',
+    'r': 'r',
+    'ř': 'R',
+    'j': 'j',
+    'p': 'p',
+    'b': 'b',
+    't': 't',
+    'd': 'd',
+    'ť': 'T',
+    'ď': 'D',
+    'k': 'k',
+    'g': 'g',
+
+    # specialities
+    'q': 'kv',
+    'w': 'v',
+
+    # nasals
+    'm': 'm',
+    'n': 'n',
+    'ň': 'J',
+
+    # affricates
+    'c': 'c',
+    'č': 'C',
+    'dz': 'w',
+    'dž': 'W',
+
+
+    # interpunction
+    '. ': '. $',
+    '.': '. $',
+    '? ': '? $',
+    '?': '? $',
+    '!': '. $',
+    '! ': '. $',
+
+    # ',': ', #',
+    ', ': ', # ',
+
+    # whitespace
+    # ' ': '|',
+
+    # glottal stops (if word starts with vocal, we add glottal stop (= !))
+    ' a': ' !a',
+    ' e': ' !e',
+    ' i': ' !i',
+    ' o': ' !o',
+    ' u': ' !u',
+
+    ' A': ' !A',
+    ' E': ' !E',
+    ' I': ' !I',
+    ' O': ' !O',
+    ' U': ' !U',
+
+    # alophone 'ch' in the end of word
+    # 'x|': 'G|',
+}
+
+# 'b', 'd', 'ď', 'g', 'v', 'z', 'ž', 'h', 'dz', 'dž', 'ř'
+VOICED_CONSONANTS_PAIR = ['b', 'd', 'D', 'g', 'v', 'z', 'Z', 'h', 'w', 'W', 'R']
+
+# 'p', 't', 'ť', 'k', 'f', 's', 'š', 'ch', 'c', 'č', 'ř'
+VOICELESS_CONSONANTS_PAIR = ['p', 't', 'T', 'k', 'f', 's', 'S', 'x', 'c', 'C', 'Q']
+
+# 'm', 'n', 'ň', 'l', 'r', 'j'
+UNIQUE_CONSONANTS = ['m', 'n', 'J', 'l', 'r', 'j']
+
+CONSONANTS_ALL = UNIQUE_CONSONANTS + VOICELESS_CONSONANTS_PAIR + VOICED_CONSONANTS_PAIR
+
+VOWELS = ['a', 'e', 'i', 'o', 'u', 'A', 'E', 'I', 'O', 'U']
+
+# Conversion VOICED_CONSONANTS_PAIR to VOICELESS_CONSONANTS_PAIR
+VOICED_CONSONANTS_PAIR_to_VOICELESS_CONSONANTS_PAIR = {
+    'b': 'p',
+    'd': 't',
+    'D': 'T',
+    'g': 'k',
+    'v': 'f',
+    'z': 's',
+    'Z': 'S',
+    'h': 'x',
+    'w': 'c',
+    'W': 'C',
+    'R': 'Q',
+}
+
+# Conversion VOICELESS_CONSONANTS_PAIR to VOICED_CONSONANTS_PAIR
+VOICELESS_CONSONANTS_PAIR_to_VOICED_CONSONANTS_PAIR = {
+    'p': 'b',
+    't': 'd',
+    'T': 'D',
+    'k': 'g',
+    'f': 'v',
+    's': 'z',
+    'S': 'Z',
+    'x': 'h',
+    'c': 'w',
+    'C': 'W',
+    'Q': 'R',
+}
+
+PERSONAL_PRONOUNS = ['nAmi', 'vAmi', 'vAs',  'nIm', 'jI', 'nI', 'jej', 'Jej',
+                    'jeho', 'Jeho', 'jimi', 'nimi']
