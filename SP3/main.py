edd45efca09ce7d97b615d58c8540622cbbc9810
janburian <jburian@students.zcu.cz>
2023-04-20 11:36:16 +0200

    First commit, updated .gitignore

---
+++ SP3/main.py
@@ -0,0 +1,42 @@
+def get_times(hours, minutes):
+    f = open("times_output.txt", 'w', encoding='utf-8')
+    for hour in hours:
+        for minute in minutes:
+            time = hour + " " + minute
+            f.write(time + '\n')
+    f.close()
+
+def get_relative_times(hours_relative):
+    f = open("rel_times_output.txt", 'w', encoding='utf-8')
+
+    hours_1 = hours_relative[0]
+    hours_2 = hours_relative[1:4]
+    hours_3 = hours_relative[4:]
+
+    for hour in hours_relative:
+        if hour in hours_1:
+            time = hour + " " + "hodinu"
+            f.write(time + '\n')
+        elif hour in hours_2:
+            time = hour + " " + "hodiny"
+            f.write(time + '\n')
+        elif hour in hours_3:
+            time = hour + " " + "hodin"
+            f.write(time + '\n')
+
+    f.close()
+
+
+hours = ["jedna", "jednu", "dvě", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět", "deset", "jedenáct", "dvanáct",
+         "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct", "dvacet", "dvacet jedna",
+         "dvacet dva", "dvacet tři"]
+
+minutes = ["pět", "nula pět", "deset", "dvacet", "dvacet pět", "třicet pět", "čtyřicet", "padesát", "padesát pět"] # vynechano patnact, tricet a ctyricet pet
+
+hours_relative = ["jednu", "dvě", "tři", "čtyři", "pět", "šest", "sedm", "osm", "devět", "deset", "jedenáct", "dvanáct",
+         "třináct", "čtrnáct", "patnáct", "šestnáct", "sedmnáct", "osmnáct", "devatenáct", "dvacet", "dvacet jedna",
+         "dvacet dva", "dvacet tři"]
+
+
+get_times(hours, minutes)
+get_relative_times(hours_relative)
